a99571afd0285fdbedf18202c3b4746ed52f7fc6
Fernando <Fernando@DESKTOP-Fernando>
2019-11-10 22:40:44 -0300

    Validação numerica no campo QtdCordenadas

--- ordenador-numerico/src/main/java/br/com/team/ordenador_numerico/aplicacao/Janela.java
+++ ordenador-numerico/src/main/java/br/com/team/ordenador_numerico/aplicacao/Janela.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -117,6 +119,24 @@ public class Janela {
 				tareaTempoExec.setText("");
 			}
 		});
+		
+		campoQtdCoord.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				
+			    char c = e.getKeyChar();
+			    if (!((c >= '0') && (c <= '9') ||
+			       (c == KeyEvent.VK_BACK_SPACE) ||
+			       (c == KeyEvent.VK_DELETE))) {
+			      e.consume();
+			    }
+				
+			}
+			public void keyPressed(KeyEvent e) {}
+			public void keyReleased(KeyEvent e) {}
+			
+		});;
 
 		return contentPane;
 
